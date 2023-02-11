06d8c409fd690936cde9cb89949d98fd3d32421f
Developer <davide.curina@gmail.com>
2023-02-11 20:39:13 +0100

    aggiunto il costruttore per consentire l'inserimento di una categori nell'articolo

--- src/main/java/it/cgmconsulting/azienda/entity/Categoria.java
+++ src/main/java/it/cgmconsulting/azienda/entity/Categoria.java
@@ -22,6 +22,10 @@ public class Categoria {
 
     private byte iva;
 
+    public Categoria(String categoria) {
+        this.categoria = categoria;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
